72fa06fb797508be8c38c65d641fcc98a64c8dd5
guid0rizzi <98404463+LeonardoGuidorizzi@users.noreply.github.com>
2023-04-27 11:23:43 -0300

    card

--- src/components/allContracts/main/index.jsx
+++ src/components/allContracts/main/index.jsx
@@ -11,7 +11,43 @@ export const ContractsPage = () => {
         <h1>Seus Contratos</h1>
       </div>
       <div className="allContracts-container">
-
+        <div>
+          <div className="card-contracts">
+            <div className="container-image">
+              <img alt="Foto motorista" />
+            </div>
+            <div className="container-all-infos">
+              <div className="nome-score">
+                <span className="nome-motorista">Joiwton</span>
+              </div>
+              <div className="infos-contratar">
+                <div className="infos-motorista">
+                  <p className="infos">Tipo de contrato: </p>
+                  <p className="infos">
+                    Escola: { }
+                  </p>
+                  <p className="infos">Passageiro:</p>
+                </div>
+                <div className="infos-endereco-container">
+                  <div className="infos-endereco-content">
+                    <p className="info-endereco">Endereço:</p>
+                    <p className="Rua-e-numero">Rua Jatoba, n 120</p>
+                    <p className="cep">06184240</p>
+                    <p className="municipio">babueri</p>
+                    <p className="bairro">Itapevi- jardim sao carlos</p>
+                  </div>
+                </div>
+                <div className="deletar-button-container">
+                  <div>
+                    <div className="button-deletar">
+                      <button>x</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   )
