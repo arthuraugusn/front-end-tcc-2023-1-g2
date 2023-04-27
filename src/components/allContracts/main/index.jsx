5dab17f9cdd8d29c67754e5ad5493e040d872572
guid0rizzi <98404463+LeonardoGuidorizzi@users.noreply.github.com>
2023-04-27 08:00:54 -0300

    titulo

--- src/components/allContracts/main/index.jsx
+++ src/components/allContracts/main/index.jsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from 'react';
 import './style.css'
 
-export const ContractsPage = () =>{
-  useEffect (()=>{
+export const ContractsPage = () => {
+  useEffect(() => {
 
   })
-  return(
+  return (
     <main className='main-container-allcontracts'>
-      
+      <div className="name-container">
+        <h1>Seus Contratos</h1>
+      </div>
+      <div className="allContracts-container">
+
+      </div>
     </main>
   )
 
