15250e7050962df05edaf6044ada136e26c012c3
guid0rizzi <98404463+LeonardoGuidorizzi@users.noreply.github.com>
2023-05-08 11:20:10 -0300

    sim

--- src/templates/NotificationsPage/body.jsx
+++ src/templates/NotificationsPage/body.jsx
@@ -1,19 +1,16 @@
 import { MainNotifications } from "../../components/Notifications-Page/main/index";
-import {FooterNotifications} from "../../components/Notifications-Page/footer/index";
+import { FooterNotifications } from "../../components/Notifications-Page/footer/index";
 import "./style.css";
 import "../reset/reset.css";
 import { useState } from "react";
 
-export const NotificationsPage = ({props}) => {
+export const NotificationsPage = ({ props }) => {
   return (
     <>
       <div className="body-notifications-page">
         <MainNotifications></MainNotifications>
-      </div>
-      <div>
         <FooterNotifications></FooterNotifications>
       </div>
-        
     </>
   );
 }; 
