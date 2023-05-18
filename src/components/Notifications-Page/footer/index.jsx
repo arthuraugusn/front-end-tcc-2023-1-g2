916c754bee19817f93cd75545b610d3849209ae9
guid0rizzi <98404463+LeonardoGuidorizzi@users.noreply.github.com>
2023-05-18 08:50:00 -0300

    mudando o footer

--- src/components/Notifications-Page/footer/index.jsx
+++ src/components/Notifications-Page/footer/index.jsx
@@ -1,50 +1,5 @@
-import "./style.css"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faWhatsapp,
-  faInstagram,
-  faYoutube,
-} from "@fortawesome/free-brands-svg-icons";
-import { Logo } from "../../Home-Page/Header/Logo/index";
-import {MenuBurguer} from "../../Home-Page/Header/MenuBurguer"
+import { FooterMotoristas } from "../../user/Motoristas-Page/Footer/index";
 
-export const FooterNotifications = ({props}) =>{
-    return(
-      <>
-      <footer>
-        <div className="contact-us">
-          <div className="icon-contact">
-            <FontAwesomeIcon icon={faWhatsapp} />
-            <span>Whatsapp</span>
-          </div>
-          <div className="icon-contact">
-            <FontAwesomeIcon icon={faInstagram} />
-            <span>Instagram</span>
-          </div>
-
-          <div className="icon-contact">
-            <FontAwesomeIcon icon={faYoutube} />
-            <span>Youtube</span>
-          </div>
-        </div>
-        <div className="footer-localization">
-          <div className="menu-burguer-footer">
-            <MenuBurguer props={{ props: props }} />
-          </div>
-          <div className="logo-footer">
-            <Logo props={{ props: props }} />
-            <span>©Copyright 2023 - Vanbora</span>
-          </div>
-        </div>
-        <div className="localization-company">
-          <div>
-            <span>Avenida dos Autonomistas, nº 1496</span>
-          </div>
-          <div>
-            <span>Vila Yara, Osasco/SP - CEP 06.020-902</span>
-          </div>
-        </div>
-      </footer>
-    </>
-    )
-}
+export const FooterNotifications = () => {
+  return <FooterMotoristas />;
+};
