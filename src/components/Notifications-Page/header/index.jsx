2a2305c5da8295340a1b14d8fac1a871254557de
guid0rizzi <98404463+LeonardoGuidorizzi@users.noreply.github.com>
2023-06-06 22:38:42 -0300

    ultimos commits

---
+++ src/components/Notifications-Page/header/index.jsx
@@ -0,0 +1,5 @@
+import { HeaderMotoristas } from "../../user/Motoristas-Page/Header";
+
+export const NotificationsHeader= () => {
+  return (<HeaderMotoristas />);
+};
--- src/templates/NotificationsPage/body.jsx
+++ src/templates/NotificationsPage/body.jsx
@@ -1,5 +1,6 @@
 import { MainNotifications } from "../../components/Notifications-Page/main/index";
 import { FooterNotifications } from "../../components/Notifications-Page/footer/index";
+import {NotificationsHeader} from "../../components/Notifications-Page/header/index"
 import "./style.css";
 import "../reset/reset.css";
 import { useState } from "react";
@@ -8,6 +9,7 @@ export const NotificationsPage = ({ props }) => {
   return (
     <>
       <div className="body-notifications-page">
+        <NotificationsHeader></NotificationsHeader>
         <MainNotifications></MainNotifications>
         <FooterNotifications></FooterNotifications>
       </div>
